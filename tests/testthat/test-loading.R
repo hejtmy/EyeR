c2972d7bae6b6b45b220942f4d796698dca0a958
hejtmy <hejtmy@gmail.com>
2019-03-04 22:55:36 +0100

    Updates tests so that they load from system files

--- tests/testthat/test-loading.R
+++ tests/testthat/test-loading.R
@@ -1,7 +1,9 @@
 context('loading')
 
-FILEPATH <- "../../inst/extdata/test-file.asc"
+DIR <- system.file("extdata", package = "eyer")
+FILEPATH <- paste0(DIR, "/test-file.asc")
 EYETRACKER <- "SR 1000"
+
 test_that('can load asc data without error',{
   df <- read_eye_fixations(FILEPATH, EYETRACKER)
   expect_equal(nrow(df), 797)
--- tests/testthat/test-preprocessing.R
+++ tests/testthat/test-preprocessing.R
@@ -1,8 +1,4 @@
 context('preprocessing')
 
 test_that('can load asc data without error',{
-  df <- read_eye_fixations(FILEPATH, EYETRACKER)
-  expect_equal(nrow(df), 797)
-  df <- read_eye_events(FILEPATH, EYETRACKER)
-  expect_equal(nrow(df), 4990)
 })
