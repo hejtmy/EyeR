5872bcfce6b90c53ba697a8af6bdf3bc5ce27539
hejtmy <hejtmy@gmail.com>
2019-03-15 14:55:12 +0100

    Changes eyelinker object description

--- R/eyelinkr-object.R
+++ R/eyelinkr-object.R
@@ -1,5 +1,21 @@
 #' Returns empty eyer object
 #'
+#' @description
+#' EyerObject is a list with predetermined fields for consistent functioning of other
+#' functions within the Eyer package. Lots of functions in the eyer package can
+#' work on passed "raw" data, such as fixation dataframes etc., but the benefit of
+#' preprocessing and parsing data inside the eyer object is that the object
+#' is inherently validated and processed and functions know what to expect. This
+#' data science design has been implemented from similar approaches found in Matlab
+#' and Python packages.
+#'
+#' EyerObject has the following fields
+#'
+#' - data
+#'    - events
+#'    - fixations
+#'    - gaze
+#'
 #' @return EyeR object
 #' @export
 #'
