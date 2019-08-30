f3bbed796873af0e43ac99ea9bf46c66fa28d325
hejtmy <hejtmy@gmail.com>
2019-08-30 03:40:50 +0200

    Adds nice visualisations of fixations

--- R/eyer-visualising.R
+++ R/eyer-visualising.R
@@ -24,6 +24,28 @@ plot_gaze <- function(obj, ...){
 plot_gaze.eyer <- function(obj, downsample = 10, ...){
   df <- obj$data$gaze
   if(downsample > 1) df <- downsample(df, downsample)
-  plt <- ggplot(df, aes(x, y)) + geom_point(...)
+  plt <- ggplot(df, aes(x, y)) + geom_point(...) + theme_minimal()
+  return(plt)
+}
+
+
+#' PLot fixations data
+#'
+#' @param obj
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_fixations <- function(obj, ...){
+  UseMethod("plot_fixations")
+}
+
+plot_fixations.eyer <- function(obj, duration = T, ...){
+  df <- obj$data$fixations
+  plt <- ggplot(df, aes(x, y)) + theme_minimal()
+  if(duration) plt <- plt + geom_point(aes(color=duration, size=duration), ...) + scale_size(guide=F)
+  if(!duration) plt <- plt + geom_point(...)
   return(plt)
 }
