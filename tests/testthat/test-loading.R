a5c08ef36a9d2d17b50dcffb1f9fb07d2b6ebfbe
Lukáš Hejtmánek <hejtmy@gmail.com>
2019-09-10 21:55:47 +0200

    closes #20 and addresses partially the #19

--- tests/testthat/test-loading.R
+++ tests/testthat/test-loading.R
@@ -4,7 +4,7 @@ test_folder <- system.file("extdata", package = "eyer")
 FIXATIONS_N_ROW <- 378
 
 test_that("Can load preprocessed data", {
-  expect_silent(eye <- load_eyer_data(test_folder))
+  expect_message(eye <- load_eyer_data(test_folder))
   expect_s3_class(eye, "eyer")
   expect_equal(FIXATIONS_N_ROW, nrow(eye$data$fixations))
 })
