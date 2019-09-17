8573f71ba6014d3031eb5ab6dc3787146a048209
hejtmy <hejtmy@gmail.com>
2019-09-18 00:43:10 +0200

    Updates how the filtering works

--- R/eyer-getters.R
+++ R/eyer-getters.R
@@ -16,8 +16,7 @@ filter_times <- function(obj, start, end, ...){
 #' Filters data between start and end time
 #'
 #' @param obj Eyer object to filted data from
-#' @param start start time (inclusive) as is in the time column
-#' @param end end time (exclusive) as is in the time column
+#' @param times is a dataframe or a numeric(2) start and end to filter parts of the eyetracking. If it is set, start and end are ignored
 #' @param data_fields list of data fields (in obj$data) to filter. If empty, all fields are filtered
 #' @param raw_times if TRUE, uses obj$info$start_time to calculate times to filter.
 #' Useful if the data have 0 based times since start, but we want to filter based on other values.
@@ -27,19 +26,28 @@ filter_times <- function(obj, start, end, ...){
 #' @export
 #'
 #' @examples
-filter_times.eyer <- function(obj, start, end, data_fields = c(), raw_times = FALSE, recalculate_times = FALSE){
+filter_times.eyer <- function(obj, times, data_fields = c(), raw_times = FALSE, recalculate_times = FALSE){
+  start <- times[1]
   start_original <- start #saves for the recalculation
   if(raw_times){
-    start <- start - obj$info$start_time
-    end <- end - obj$info$start_time
+    times <- times - obj$info$start_time
   }
   if(recalculate_times){
     obj$info$start_time <- obj$info$start_time + start
+    ### DOESN'T WORK!
   }
   if(length(data_fields) == 0) data_fields <- names(obj$data)
   for(field in data_fields){
     df <- obj$data[[field]]
-    obj$data[[field]] <-  df[df$time >= start & df$time < end, ]
+    if(!is.data.frame(times)){
+      obj$data[[field]] <-  df[df$time >= times[1] & df$time < times[2], ]
+      next
+    }
+    df_filtered <- data.frame()
+    for(i in 1:nrow(times)){
+      df_filtered <- rbind(df_filtered, df[df$time >= times$start[i] & df$time < times$end[i], ])
+    }
+    obj$data[[field]] <- df_filtered
   }
   return(obj)
 }
--- tests/testthat/test-getters.R
+++ tests/testthat/test-getters.R
@@ -2,17 +2,34 @@ context("getters work")
 
 obj <- eyer_data
 
-test_that("filtering works", {
+test_that("filtering single time works", {
   START_TIME <- obj$info$start_time
 
-  expect_silent(filtered <- filter_times(obj, 0, 10))
+  expect_silent(filtered <- filter_times(obj, c(0, 10)))
   expect_s3_class(filtered, "eyer")
   expect_equal(nrow(filtered$data$events), 0)
 
-  expect_silent(filtered <- filter_times(obj, 0, 10, data_fields = "gaze"))
+  expect_silent(filtered <- filter_times(obj, c(0, 10), data_fields = "gaze"))
   expect_equal(nrow(filtered$data$events), 1)
 
-  expect_silent(filtered_raw <- filter_times(obj, START_TIME, START_TIME + 10, raw_times = T))
-  expect_silent(filtered <- filter_times(obj, 0, 10, raw_times = F))
+  expect_silent(filtered_raw <- filter_times(obj, c(START_TIME, START_TIME + 10), raw_times = T))
+  expect_silent(filtered <- filter_times(obj, c(0, 10), raw_times = F))
+  expect_equal(filtered_raw, filtered)
+})
+
+test_that("filtering dataframes works", {
+  START_TIME <- obj$info$start_time
+
+  times <- data.frame(start=c(0,50,100), end=c(10,70,110))
+
+  expect_silent(filtered <- filter_times(obj, times))
+  expect_s3_class(filtered, "eyer")
+  expect_equal(nrow(filtered$data$events), 0)
+
+  expect_silent(filtered <- filter_times(obj, c(0, 10), data_fields = "gaze"))
+  expect_equal(nrow(filtered$data$events), 1)
+
+  expect_silent(filtered_raw <- filter_times(obj, times+START_TIME , raw_times = T))
+  expect_silent(filtered <- filter_times(obj, times, raw_times = F))
   expect_equal(filtered_raw, filtered)
 })
